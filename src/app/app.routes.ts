837b0481d59598ff2edb82e53b4a86389ce13122
DionatanLeao <dionatan.ti@outlook.com>
2025-07-26 21:16:51 -0300

    feat: Created Home Component part 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: NavComponent,
+        component: NavComponent, 
+        children: [
+            { path: 'home', component: HomeComponent },
+            { path: '', redirectTo: 'home', pathMatch: 'full'}
+        ]
     }
-    
 ];
