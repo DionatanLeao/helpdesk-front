1f5ecdf056e47dc10dccfe5921c5b641b0cd2fb9
DionatanLeao <dionatan.ti@outlook.com>
2025-07-28 20:50:38 -0300

    feat: HEADER Component

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -3,12 +3,13 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import { MatNavList } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { Router, RouterModule } from '@angular/router';
+import { HeaderComponent } from "../header/header.component";
 
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [MatSidenavModule, MatNavList, MatIconModule, RouterModule],
+  imports: [MatSidenavModule, MatNavList, MatIconModule, RouterModule, HeaderComponent],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
