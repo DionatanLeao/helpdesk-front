38ab99d30c7f96ef5b97607573fde645bc34f26e
DionatanLeao <dionatan.ti@outlook.com>
2025-08-05 23:25:23 -0300

    feat: LOGIN Component - Part 3 and implements logout

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Credenciais } from '../models/Credenciais';
+import { HttpClient } from '@angular/common/http';
+import { API_CONFIG } from '../config/api.config';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  jwtService: JwtHelperService = new JwtHelperService()
+
+  constructor(private httpClient: HttpClient) { }
+
+  authenticate(creds: Credenciais) {
+    return this.httpClient.post(`${API_CONFIG.baseUrl}/login`, creds, {
+      observe: 'response',
+      responseType: 'text'
+    })
+  }
+
+  successfullLogin(authToken: string) {
+    localStorage.setItem('token', authToken)
+  }
+
+  isAuthenticated() {
+    let token = localStorage.getItem('token')
+    if(token != null) {
+        return !this.jwtService.isTokenExpired(token)
+    }
+    return false
+  }
+
+  logout() {
+    localStorage.clear()
+  }
+}
