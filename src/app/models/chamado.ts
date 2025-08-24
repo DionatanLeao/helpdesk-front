b1967885ebfacfc13db6a9b08ca5fc6bc953c1f5
DionatanLeao <dionatan.ti@outlook.com>
2025-08-24 11:52:05 -0300

    feat: Component ticket-list part 1

---
+++ src/app/models/chamado.ts
@@ -0,0 +1,13 @@
+export interface Chamado {
+    id?: any,
+    openingDate?: string;
+    closingDate?: string;
+    priority: string;
+    status: string;
+    title: string;
+    observations: string;
+    technician: any;
+    client: any;
+    clientName: string;
+    technicianName: string;
+}
---
+++ src/app/services/chamado.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { API_CONFIG } from '../config/api.config';
+import { Observable } from 'rxjs';
+import { Chamado } from '../models/chamado';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChamadoService {
+constructor(private httpClient: HttpClient) { }
+
+  findById(id: any): Observable<Chamado> {
+    return this.httpClient.get<Chamado>(`${API_CONFIG.baseUrl}/tickets/${id}`)
+  }
+
+  findAll(): Observable<Chamado[]> {
+    return this.httpClient.get<Chamado[]>(`${API_CONFIG.baseUrl}/tickets`)
+  }
+
+  create(chamado: Chamado): Observable<Chamado> {
+    return this.httpClient.post<Chamado>(`${API_CONFIG.baseUrl}/tickets`, chamado)
+  }
+
+  update(chamado: Chamado): Observable<Chamado> {
+    return this.httpClient.put<Chamado>(`${API_CONFIG.baseUrl}/tickets/${chamado.id}`, chamado)
+  }
+
+  delete(id: any): Observable<Chamado> {
+    return this.httpClient.delete<Chamado>(`${API_CONFIG.baseUrl}/clients/${id}`)
+  }
+}
